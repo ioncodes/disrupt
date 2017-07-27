3eba3c13edfdd08ca36d6aa651c9c49268046bad
Luca Marcelli <ionix@protonmail.com>
2017-07-27 21:23:09 +0200

    add file reader

--- disrupt.js
+++ disrupt.js
@@ -2,11 +2,14 @@
 
 const commander = require('commander');
 const colors = require('colors');
+const fs = require('fs');
 const Disassembler = require('./lib/disassembler.js');
+const Reader = require('./lib/reader.js');
 
 commander
   .version('0.0.1')
   .option('-f, --file <file>', 'file to be disassembled')
+  .option('-l, --length <length>', 'amount of bytes to read from the specified offset')
   .option('-b, --buffer <buffer>', 'read buffer from arg')
   .option('-o, --offset <offset>', 'offset to read from')
   .option('-a, --arch <arch>', 'file architecture')
@@ -18,6 +21,7 @@ if (commander.verbose) {
   console.log('File:   ' + commander.file);
   console.log('Buffer: ' + commander.buffer);
   console.log('Offset: ' + commander.offset);
+  console.log('Length: ' + commander.length);
   console.log('Arch:   ' + commander.arch);
   console.log('Mode:   ' + commander.mode);
 }
@@ -33,6 +37,11 @@ process.on('exit', function() {
 
 if (commander.buffer) {
   var buffer = commander.buffer.split(',').map(Number);
+  var instructions = disassembler.disassemble(Buffer.from(buffer), parseInt(commander.offset));
+  print(instructions);
+} else {
+  var reader = new Reader(fs.readFileSync(commander.file));
+  var buffer = reader.read(parseInt(commander.offset), parseInt(commander.length));
   var instructions = disassembler.disassemble(Buffer.from(buffer), parseInt(commander.offset));
   print(instructions);
 }
