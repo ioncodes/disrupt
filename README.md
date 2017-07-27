e04b27e643de0a807b68a51d52db51e80b637d7b
Luca Marcelli <ionix@protonmail.com>
2017-07-27 16:20:36 +0200

    make buffer work and add readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-disrupt
+# disrupt
+disrupt is a small commandline utility to disassemble files quickly. You can set the architecture, mode and assembly syntax. Additionally, parsing a buffer from the commandline is possible aswell.
+
+# Example
+```disrupt -a X86 -m 64 -o 0x1337 -b '0x55,0x48,0x8b,0x05,0xb8,0x13,0x00,0x00'``` gives you this:  
+```asm
+0x1337: push    rbp
+0x1338: mov     rax, qword ptr [rip + 0x13b8]
+```
+
+# Help
+Running ```disrupt --help``` shows you the help menu:  
+```
+  Usage: disrupt [options]
+
+
+  Options:
+
+    -V, --version          output the version number
+    -f, --file <file>      file to be disassembled
+    -b, --buffer <buffer>  read buffer from arg
+    -o, --offset <offset>  offset to read from
+    -a, --arch <arch>      file architecture
+    -m, --mode <mode>      file mode
+    -v, --verbose          verbose output
+    -h, --help             output usage information
+```
--- disrupt.js
+++ disrupt.js
@@ -5,17 +5,20 @@ const Disassembler = require('./lib/disassembler.js');
 
 commander
   .version('0.0.1')
-  .usage('-f <file> -a <arch> -m <mode> [optional]')
   .option('-f, --file <file>', 'file to be disassembled')
+  .option('-b, --buffer <buffer>', 'read buffer from arg')
+  .option('-o, --offset <offset>', 'offset to read from')
   .option('-a, --arch <arch>', 'file architecture')
   .option('-m, --mode <mode>', 'file mode')
   .option('-v, --verbose', 'verbose output')
   .parse(process.argv);
 
 if(commander.verbose) {
-  console.log('File: ' + commander.file);
-  console.log('Arch: ' + commander.arch);
-  console.log('Mode: ' + commander.mode);
+  console.log('File:   ' + commander.file);
+  console.log('Buffer: ' + commander.buffer);
+  console.log('Offset: ' + commander.offset);
+  console.log('Arch:   ' + commander.arch);
+  console.log('Mode:   ' + commander.mode);
 }
 
 const disassembler = new Disassembler(commander.arch, commander.mode);
@@ -24,4 +27,7 @@ process.on('exit', function() {
   disassembler.close();
 });
 
-disassembler.disassemble(new Buffer([ 0x55, 0x48, 0x8b, 0x05, 0xb8, 0x13, 0x00, 0x00 ]), 0x1337);
+if(commander.buffer) {
+  var buffer = commander.buffer.split(',').map(Number);
+  disassembler.disassemble(Buffer.from(buffer), parseInt(commander.offset));
+}
